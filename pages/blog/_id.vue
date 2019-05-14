2fafa2a0f1f26ece82614065fd65b958c596b101
Baptiste BRIOIS <baptiste.briois@hotmail.fr>
2019-05-14 09:25:43 +0200

    Initial project

---
+++ pages/blog/_id.vue
@@ -0,0 +1,44 @@
+<template>
+    <div v-if="currentArticle" class="container">
+        <h1>{{ currentArticle.name }}</h1>
+
+        <p>{{ currentArticle.description }}</p>
+
+        <nuxt-link :to="{name: 'blog'}">
+            <button>Retour</button>
+        </nuxt-link>
+    </div>
+</template>
+
+<script>
+    import axios from "~/plugins/axios";
+
+    export default {
+        async asyncData({ params }) {
+            const { data } = await axios.get(`/servers/${params.id}`);
+
+            return {
+                currentArticle: data
+            }
+        },
+
+        data() {
+            return {
+                currentArticle: null
+            }
+        },
+
+        head() {
+            return {
+                title: `${this.currentArticle.name} | Mon site internet`,
+                meta: [
+                    {}
+                ]
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <h2>{{ title }}</h2>
+        <ul>
+            <li v-for="article in articles" :key="article.id">
+                <nuxt-link :to="{name: 'blog-id', params: {id: article.id}}">
+                    {{ article.name }}
+                </nuxt-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import axios from "~/plugins/axios";
+
+    export default {
+        async asyncData() {
+            const { data } = await axios.get('/servers');
+            return {
+                articles: data
+            }
+        },
+
+        data() {
+            return {
+                title: "Mes articles",
+                articles: []
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .container {
+        width: 980px;
+        margin: 0 auto;
+    }
+</style>

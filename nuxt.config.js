01f76782ac9deedc1a289722d55758067c746f80
Baptiste BRIOIS <baptiste.briois@hotmail.fr>
2019-05-14 09:56:52 +0200

    Bootstrap js cdn

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,9 +22,9 @@ export default {
       { rel: 'stylesheet', href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' }
     ],
     script: [
-      { src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js' },
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js' },
-      { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js' }
+      { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' },
+      { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' }
     ],
   },
 

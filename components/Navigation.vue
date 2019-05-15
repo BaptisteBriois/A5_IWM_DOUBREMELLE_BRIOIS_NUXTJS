ac6702736585b25ae048a6b687558d6a00f6427a
Baptiste BRIOIS <baptiste.briois@hotmail.fr>
2019-05-15 15:32:08 +0200

    Update home style

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
         <div class="container">
-            <nuxt-link :to="{name: 'index'}" class="navbar-brand">Start Bootstrap</nuxt-link>
+            <nuxt-link :to="{name: 'index'}" class="navbar-brand">BlogHeure</nuxt-link>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
                 <i class="fas fa-bars"></i>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,23 @@
 <template>
     <header class="masthead">
         <div class="overlay"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-md-10 mx-auto">
-                    <div class="site-heading">
-                        <h1>Clean Blog</h1>
-                        <span class="subheading">A Blog Theme by Start Bootstrap</span>
-                    </div>
+        <div class="content d-flex align-items-center justify-content-center">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="site-heading">
+                    <h1>Notre blog</h1>
+                    <span class="subheading">Voici notre blog</span>
                 </div>
             </div>
         </div>
     </header>
 </template>
+
+<style scoped>
+    .masthead {
+        height: 100vh;
+        margin-bottom: 0;
+    }
+    .content {
+        height: 100vh;
+    }
+</style>
